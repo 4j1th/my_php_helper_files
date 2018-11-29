2e057136f67f41fc5690a811b0e7e0e98fa78dcd
Ajith Abraham <ajithabraham.ma@gmail.com>
2018-11-29 11:14:31 +0530

    CodeIgniter development model files

---
+++ CI_controler_modal.php
@@ -0,0 +1,59 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	 
+	public function __construct()
+	{
+		parent::__construct();
+		// session_start();
+		$this->load->library('session');
+		
+		$_SESSION['userid'] = '3'; $_SESSION['sellerid'] = '1';
+		
+
+		// Is the User logged?
+		if (!isset($_SESSION['userid'])) { 
+			
+			$this->output
+		    ->set_status_header(200)
+		    ->set_content_type('application/json', 'utf-8')
+		    ->set_output(json_encode('Unauthorized Access'))
+		    ->_display();
+			exit; 
+		}
+	}
+	
+	public function index()
+	{
+		
+		$post = $this->input->post(NULL, TRUE);
+
+		$this->load->model('searchdb');
+		$result = $this->searchdb->keyword_search($post);
+
+		$this->output
+		    ->set_status_header(200)
+		    ->set_content_type('application/json', 'utf-8')
+		    ->set_output(json_encode($result))
+		    ->_display();
+        
+		exit;
+	}
+}
---
+++ CI_model_db.php
@@ -0,0 +1,50 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * MediaLinks
+ *
+ * A MediaLinks system
+ *
+ * @package		MediaLinks
+ * @author		MediaLinks Dev Team, Ajith Abraham
+ * @copyright	Copyright (c) 2016 - 2017, MediaLinks
+ * @license		
+ * @link		
+ * @since		Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * Modeldb Class
+ *
+ * @package		MediaLinks
+ * @subpackage	Model
+ * @category	Model
+ * @author		MediaLinks Dev Team, Ajith Abraham
+ * @link		https://medialinks.in
+ */
+class Modeldb extends CI_Model {
+
+	/**
+	 * Constructor
+	 * 
+	 */
+	public function __construct()
+	{
+		$this->load->database();
+	}
+
+	/**
+	 * Fuction Description
+	 */
+	public function dob_check($dob, $athmasthithiId)
+	{
+		
+	}
+
+}
+// END Login Class
+
+/* End of file login.php */
+/* Location: ./application/models/login.php */
